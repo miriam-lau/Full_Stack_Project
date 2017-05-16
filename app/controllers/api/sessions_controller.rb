4f52d8b917a3bf26cc6c2ce50aaf4eb295f65574
miriam-lau <meiyen.lau@gmail.com>
2017-05-16 11:26:35 -0700

    Complete sessions controller create and destroy method

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,24 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username],
+      params[:user][:password])
+
+    if @user
+      sign_in(@user)
+      render "api/users/show"
+    else
+      render(json: ["Invalid Credentials"], status: 401)
+    end
   end
 
   def destroy
+    @user = current_user
+    if @user
+      sign_out
+      #should it render to index?
+      render "api/users/show"
+    else
+      render(json: ["Not Signed In"], status: 404)
+    end
   end
 end

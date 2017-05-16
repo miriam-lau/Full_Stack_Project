72743b2048357e122fd2191e1358491923dc923c
miriam-lau <meiyen.lau@gmail.com>
2017-05-16 12:48:28 -0700

    Create session_api_util file and confirm is working

--- frontend/pantry.jsx
+++ frontend/pantry.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { signup, signin, signout } from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+
+    window.signup = signup;
+    window.signin = signin;
+    window.signout = signout;
+
     ReactDOM.render(<h1>Hello at Entry</h1>, root);
 });

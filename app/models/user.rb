6e11c1a55dcd8c539e47bf3ab7c27cac9db86cad
miriam-lau <meiyen.lau@gmail.com>
2017-05-16 09:49:14 -0700

    Add bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,31 @@
 class User < ApplicationRecord
+  validates :username, :email, :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+  after_intialization :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?
+
+  end
+
+  def self.find_by_credentials
+
+  end
+
+
+  private
+
+  def ensure_session_token
+
+  end
+
+  def reset_token!
+
+  end
 end

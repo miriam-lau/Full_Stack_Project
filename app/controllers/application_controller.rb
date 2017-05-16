773a634ccd6f8d453b9dcf3afa3a494a58cd0306
miriam-lau <meiyen.lau@gmail.com>
2017-05-16 10:49:48 -0700

    Add controllers static_pages, static_pages view, and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method: current_user, signed_in?
+
+  private
+  def current_user(user)
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_token!
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_out
+    current_user.try(:reset_token!)
+    session[:session_token] = nil
+  end
+
+  #do i need this one?
+  # def require_signed_in!
+  #
+  # end
 end

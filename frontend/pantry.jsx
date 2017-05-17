36d11d48dfe346547e1d6862662a0487e4675eb6
miriam-lau <meiyen.lau@gmail.com>
2017-05-16 17:27:03 -0700

    Complete bootstrapping of User

--- frontend/pantry.jsx
+++ frontend/pantry.jsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { signup, signin, signout } from './util/session_api_util';
-
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    let store = configureStore();
-
+    let store;
+    if (window.currentUser) {
+      const preloadedState = {session: {currentUser: window.currentUser, errors: [] }};
+      store = configureStore(preloadedState);
+      delete window.currentUser;
+    } else {
+      store = configureStore();
+    }
     window.store = store;
-    window.signup = signup;
-    window.signin = signin;
-    window.signout = signout;
 
+    const root = document.getElementById('root');
     ReactDOM.render(<Root store={ store } />, root);
 });

b282bc74f954aa4d25c52bb13e66fa1698020534
miriam-lau <meiyen.lau@gmail.com>
2017-05-18 09:45:04 -0700

    Add postinstall to package.json

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import PantryItemsReducer from './pantry_items_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer

9ff6c3e66e949539002f7dfeb787f84b15487ecb
miriam-lau <meiyen.lau@gmail.com>
2017-06-02 11:53:58 -0700

    Modified root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
-import SessionReducer from './session_reducer';
-import PantryItemsReducer from './pantry_items_reducer';
 import GroceryReducer from './grocery_reducer';
+import PantryItemsReducer from './pantry_items_reducer';
+import RecipeReducer from './recipe_reducer';
 import SearchReducer from './search_reducer';
+import SessionReducer from './session_reducer';
 
 const rootReducer = combineReducers({
-  session: SessionReducer,
-  pantry_items: PantryItemsReducer,
   grocery: GroceryReducer,
-  search: SearchReducer
+  pantry_items: PantryItemsReducer,
+  recipe: RecipeReducer,
+  search: SearchReducer,
+  session: SessionReducer
 });
 
 export default rootReducer;

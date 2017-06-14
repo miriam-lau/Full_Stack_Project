9dad73c0be3de720cc5e12c50ab90f306760be80
Miriam Lau <meiyen.lau@gmail.com>
2017-06-14 10:17:10 -0700

    Create recipe form

--- frontend/components/recipe/recipe_form.jsx
+++ frontend/components/recipe/recipe_form.jsx
@@ -1,41 +1,72 @@
 import React from 'react';
+import { Divider, Paper, TextField } from 'material-ui';
 
 class RecipeForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { name: '', image_url: '', link: '', serving: 0,
+      rating: 0, description: '', directions: '', notes: ''}
+  }
+
+  update(property) {
+    return event => this.setState({ [property]: event.target.value });
   }
 
   render() {
     return (
       <form className="recipe-form" onSubmit={this.handleSubmit}>
-        <label className="recipe-form-label">Add New Recipe</label>
-        <br />
+        <h2 className="recipe-form-label">Add New Recipe</h2>
+        <section className="recipe-form-input">
+          <div>
+            <span>Recipe Name</span>
+            <input type="text" value={this.state.name} placeholder="Name"
+              onChange={this.update('name')} />
+          </div>
+          <div>
+            <span>Recipe Image</span>
+            <input type="file" accept="image/*" placeholder="Upload an image"
+              styles="display: none;" />
+          </div>
+          <div>
+            <span>Website URL</span>
+            <input type="text" value={this.state.link} placeholder="URL"
+              onChange={this.update('link')} />
+          </div>
+          <div>
+            <span>Servings</span>
+            <input type="number" value={this.state.serving} placeholder="Number of Servings"
+              onChange={this.update('serving')} />
+          </div>
+          <div>
+            <span>Rating</span>
+            <input type="number" value={this.state.rating} placeholder="Stars"
+              onChange={this.update('rating')} />
+          </div>
+          <div>
+            <span>Short Description</span>
+            <input type="text" value={this.state.description} placeholder="Description"
+              onChange={this.update('description')} />
+          </div>
+          <div>
+            <span>Directions</span>
+            <input type="text" value={this.state.directions} placeholder="Directions"
+              onChange={this.update('directions')} />
+          </div>
+          <div>
+            <span>Cooking Notes</span>
+            <input type="text" value={this.state.notes} placeholder="Notes"
+              onChange={this.update('notes')} />
+          </div>
 
-        <div>name</div>
-        <div>serving</div>
-        <div>nutrition</div>
-        <div>rating</div>
-        <div className="new-recipe">
-          <h3>Description</h3>
-        </div>
-        <div>directions</div>
-        <div>notes</div>
-        <div>link</div>
+          <div>
+            <span>Save Recipe</span>
+            <button>Save Recipe</button>
+          </div>
+        </section>
+
       </form>
     )
   }
 }
 
 export default RecipeForm;
-
-
-
-
-
-// <TextField id="text-field-default"
-// value={this.state.temp}
-// underlineFocusStyle ={textboxUnderlineStyle}
-// style={addItemTextBoxStyle}
-// hintText="e.g. '2 Oranges' or '3 cups Milk'"
-// onChange={this.update('temp')}
-// />

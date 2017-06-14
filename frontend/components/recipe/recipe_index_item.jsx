a1c5771adcec5834ef5a77ad849b72313ff79a3f
Miriam Lau <meiyen.lau@gmail.com>
2017-06-14 09:04:09 -0700

    Render recipes onto recipe index

--- frontend/components/recipe/recipe_index_item.jsx
+++ frontend/components/recipe/recipe_index_item.jsx
@@ -6,8 +6,18 @@ class RecipeIndexItem extends React.Component{
   }
 
   render() {
+    const recipe = this.props.recipe;
     return (
-      <div>Each Recipe here</div>
+      <div>
+        <div>{recipe.name}</div>
+        <div>Servings: {recipe.serving}</div>
+        <div>Nutrition: {recipe.nutrition}</div>
+        <div>Rating: {recipe.rating}</div>
+        <div>Description: {recipe.description}</div>
+        <div>Directions: {recipe.directions}</div>
+        <div>Notes: {recipe.notes}</div>
+        <div>Link: {recipe.link}</div>
+      </div>
     );
   }
 }

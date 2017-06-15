8f35ba8b4810b155a641939b1e24c3afa5e471f3
Miriam Lau <meiyen.lau@gmail.com>
2017-06-15 10:58:50 -0700

    Update home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -26,25 +26,31 @@ class Home extends React.Component {
           autoInterval={5000}>
 
           <div className="home-panels">
-            <h2 className="home-titles">myPantry</h2>
+            <h2 className="home-titles1">myPantry</h2>
             <img src="https://images.unsplash.com/photo-1494390248081-4e521a5940db?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1079&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">myPantry</h2>
+            <h2 className="home-titles">Keep Track of Your Pantry</h2>
             <img src="https://images.unsplash.com/photo-1492739159057-7d1896b3c63f?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=844&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">myPantry</h2>
+            <h2 className="home-titles">Auto-Update Your Pantry</h2>
             <img src="https://images.unsplash.com/photo-1482012792084-a0c3725f289f?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
              alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">myPantry</h2>
+            <h2 className="home-titles">Make Grocery Lists</h2>
+            <img src="https://images.unsplash.com/photo-1491994778642-b286df793bf6?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
+              alt="splash-img"/>
+          </div>
+
+          <div>
+            <h2 className="home-titles">Save Recipes</h2>
             <img src="https://images.unsplash.com/photo-1491994778642-b286df793bf6?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>

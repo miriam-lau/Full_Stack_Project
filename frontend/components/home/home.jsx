423e1ebdbc23c6b13b4d37603f48a395a9a79a5a
Miriam Lau <meiyen.lau@gmail.com>
2017-06-15 10:35:53 -0700

    Update home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import HomeContainer from './home_container';
 import Carousel from 'nuka-carousel';
+// import Decorator from './decorator';
 
 class Home extends React.Component {
   constructor(props) {
@@ -25,29 +26,48 @@ class Home extends React.Component {
           autoInterval={5000}>
 
           <div className="home-panels">
-            <h2 className="home-titles">Keep Track of Your Pantry</h2>
+            <h2 className="home-titles">myPantry</h2>
             <img src="https://images.unsplash.com/photo-1494390248081-4e521a5940db?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1079&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">Auto-Update Your Pantry</h2>
+            <h2 className="home-titles">myPantry</h2>
             <img src="https://images.unsplash.com/photo-1492739159057-7d1896b3c63f?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=844&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">Make Grocery Lists</h2>
+            <h2 className="home-titles">myPantry</h2>
             <img src="https://images.unsplash.com/photo-1482012792084-a0c3725f289f?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
              alt="splash-img"/>
           </div>
 
           <div>
-            <h2 className="home-titles">Auto-Generate Grocery Lists</h2>
+            <h2 className="home-titles">myPantry</h2>
             <img src="https://images.unsplash.com/photo-1491994778642-b286df793bf6?dpr=2&amp;auto=format&amp;fit=crop&amp;w=1500&amp;h=1000&amp;q=80&amp;cs=tinysrgb&amp;crop=&amp;bg="
               alt="splash-img"/>
           </div>
-       </Carousel>
+        </Carousel>
+
+        <div className="app-info">
+          <section>
+            <h3>Keep Track of Your Pantry</h3>
+            <p>hello</p>
+          </section>
+          <section>
+            <h3>Auto-Update Your Pantry</h3>
+            <p>hello</p>
+          </section>
+          <section>
+            <h3>Make Grocery Lists</h3>
+            <p>hello</p>
+          </section>
+          <section>
+            <h3>Save Recipes</h3>
+            <p>hello</p>
+          </section>
+        </div>
 
        <div id="footer">
          <footer className="home-footer">

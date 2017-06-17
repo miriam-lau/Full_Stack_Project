9cea4f970a89246539bc239a55bd0b4cd276b1f8
Miriam Lau <meiyen.lau@gmail.com>
2017-06-16 17:36:08 -0700

    Add pic to recipe show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,7 +49,8 @@ recipe1 = Recipe.create(name: "Fettuccine Alfredo", serving: 4,
   ingredients: "1 lb. pasta\n1 jar alfredo sauce\nSalt and pepper",
   rating: 4.8, description: "", directions: "1. Boil pasta.\n
   2. Heat up alfredo sauce.\n3. Mix together.\n.4. Season with salt and pepper.",
-  notes: "", link: "", user_id: guest.id)
+  notes: "", link: "http://www.thefruitdoctor.com/wp-content/uploads/2014/07/fettuccine-alfredo_456X342_0.jpg",
+  user_id: guest.id)
 recipe2 = Recipe.create(name: "Chicken Pot Pie", serving: 6,
   ingredients: "", rating: 5.0, description: "", directions: "", notes: "",
   link: "", user_id: guest.id)

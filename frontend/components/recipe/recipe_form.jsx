19fc6134e45cb892db8a72deb5af35bdca18a9d7
Miriam Lau <meiyen.lau@gmail.com>
2017-06-19 13:18:10 -0700

    Update new recipe page

--- frontend/components/recipe/recipe_form.jsx
+++ frontend/components/recipe/recipe_form.jsx
@@ -29,53 +29,61 @@ class RecipeForm extends React.Component {
         </div>
         <section className="recipe-form-input">
           <div>
-            <span>Recipe Name</span>
+            <span>Name</span>
             <input type="text" value={this.state.name} placeholder="Name"
               onChange={this.update('name')} />
           </div>
-          <div>
-            <span>Recipe Image</span>
-            <div className="upload-img">
-              <input type="file" accept="image/*" name="Upload an Image" />
+
+          <div className="recipe-form-part1">
+            <div className="recipe-form-part4">
+              <span>Image</span>
+              <div className="upload-img">
+                <input type="file" accept="image/*" name="Upload an Image" />
+              </div>
+            </div>
+            <div className="recipe-form-part3">
+              <div className="recipe-form-part2">
+                <span>Servings</span>
+                <input className="input-field" type="number" value={this.state.serving} placeholder="Number of Servings"
+                  onChange={this.update('serving')} />
+              </div>
+              <div className="recipe-form-part2">
+                <span>Rating</span>
+                <input className="input-field" type="number" value={this.state.rating} placeholder="Stars"
+                  onChange={this.update('rating')} />
+              </div>
+              <div className="recipe-form-part2">
+                <span>Website</span>
+                <input className="input-field" type="text" value={this.state.link} placeholder="URL"
+                  onChange={this.update('link')} />
+              </div>
             </div>
           </div>
+
           <div>
-            <span>Website URL</span>
-            <input type="text" value={this.state.link} placeholder="URL"
-              onChange={this.update('link')} />
+            <span>Description</span>
+            <textarea name="description" value={this.state.description}
+              placeholder="Short Description" rows="3"
+              onChange={this.update('description')} />
           </div>
-          <div>
-            <span>Servings</span>
-            <input type="number" value={this.state.serving} placeholder="Number of Servings"
-              onChange={this.update('serving')} />
-          </div>
-          <div>
-            <span>Rating</span>
-            <input type="number" value={this.state.rating} placeholder="Stars"
-              onChange={this.update('rating')} />
-          </div>
+
           <div>
             <span>Ingredients</span>
             <textarea name="ingredients" value={this.state.ingredients}
-              placeholder="Ingredients" rows="5"
+              placeholder="Ingredients" rows="7"
               onChange={this.update('ingredients')} />
           </div>
-          <div>
-            <span>Short Description</span>
-            <textarea name="description" value={this.state.description}
-              placeholder="Description" rows="5"
-              onChange={this.update('description')} />
-          </div>
+
           <div>
             <span>Directions</span>
             <textarea name="directions" value={this.state.directions}
-              placeholder="Directions" rows="10"
+              placeholder="Directions" rows="7"
               onChange={this.update('directions')} />
           </div>
           <div>
-            <span>Cooking Notes</span>
+            <span>Notes</span>
             <textarea name="notes" value={this.state.notes}
-              placeholder="Notes" rows="5"
+              placeholder="Cooking Notes" rows="3"
               onChange={this.update('notes')} />
           </div>
 

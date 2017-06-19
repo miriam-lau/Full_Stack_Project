5cda0ed7ad6d1d88310140ce448a83b736fb00cd
Miriam Lau <meiyen.lau@gmail.com>
2017-06-18 18:32:12 -0700

    Refactor recipe index page

--- frontend/components/recipe/recipe_index.jsx
+++ frontend/components/recipe/recipe_index.jsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 
-import {GridList, GridTile} from 'material-ui/GridList';
-
-import Subheader from 'material-ui/Subheader';
-
-
 import RecipeIndexItem from './recipe_index_item';
 import RecipeDetailContainer from './recipe_detail_container';
-import RecipeFormContainer from './recipe_form_container';
-
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-
-    width: 5000,
-    height: 450,
-    overflowY: 'auto',
-  },
-};
 
 class RecipeIndex extends React.Component {
   constructor(props) {

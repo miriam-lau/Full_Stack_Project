abef5a55b9700bb66a141e1671dd1b48a2dca13c
Miriam Lau <meiyen.lau@gmail.com>
2017-06-20 07:49:14 -0700

    Add error handling for search bar

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ErrorBanner(props) {
-  if (this.props.searchItems.length === 0) {
-    return (
-      <div>No items found. Try again.</div>
-    );
-  } else {
-    return null;
-  }
-}
-
 class SearchResults extends React.Component {
   constructor(props) {
     super(props);
     this
   }
 
-  // errorMessage() {
-  //   if (this.props.searchItems.length === 0) {
-  //     return "No items found. Try again."
-  //   } else {
-  //     return null
-  //   }
-  // }
+  ErrorMessage() {
+    if (this.props.searchItems.length === 0) {
+      return (
+        <div>No items found. Try again.</div>
+      );
+    } else {
+      return null;
+    }
+  }
 
   render() {
     console.log("in search results");
     console.log(this.props.searchItems);
     return(
       <div>
-        <ErrorBanner />
+        {this.ErrorMessage()}
         {this.props.searchItems.map((item) => {
           return(
             <div className="search-item-info">

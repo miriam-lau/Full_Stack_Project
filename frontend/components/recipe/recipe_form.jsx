746986949419b976a0b39843c49d24c66b9385f2
Miriam Lau <meiyen.lau@gmail.com>
2017-06-25 20:53:56 -0700

    Add background to side nav for new recipe

--- frontend/components/recipe/recipe_form.jsx
+++ frontend/components/recipe/recipe_form.jsx
@@ -21,78 +21,82 @@ class RecipeForm extends React.Component {
 
   render() {
     return (
-      <form className="recipe-form" onSubmit={this.handleSubmit}>
-        <div className="new-recipe-title">
-          <h2>New Recipe</h2>
-        </div>
-        <section className="recipe-form-input">
-          <div>
-            <span>Name</span>
-            <input type="text" value={this.state.name} placeholder="Name"
-              onChange={this.update('name')} />
-          </div>
+      <div>
+        <div className="recipe-form-nav-bar"></div>
 
-          <div className="recipe-form-part1">
-            <div className="recipe-form-part4">
-              <span>Image</span>
-              <div className="upload-img">
-                <label className="upload-file">Upload an Image</label>
-                <input id="file" type="file" accept="image/*" />
+        <form className="recipe-form" onSubmit={this.handleSubmit}>
+          <div className="new-recipe-title">
+            <h2>New Recipe</h2>
+          </div>
+          <section className="recipe-form-input">
+            <div>
+              <span>Name</span>
+              <input type="text" value={this.state.name} placeholder="Name"
+                onChange={this.update('name')} />
+            </div>
+
+            <div className="recipe-form-part1">
+              <div className="recipe-form-part4">
+                <span>Image</span>
+                <div className="upload-img">
+                  <label className="upload-file">Upload an Image</label>
+                  <input id="file" type="file" accept="image/*" />
+                </div>
+              </div>
+              <div className="recipe-form-part3">
+                <div className="recipe-form-part2">
+                  <span>Servings</span>
+                  <input className="input-field" type="number" value={this.state.serving} placeholder="Number of Servings"
+                    onChange={this.update('serving')} />
+                </div>
+                <div className="recipe-form-part2">
+                  <span>Rating</span>
+                  <input className="input-field" type="number" value={this.state.rating} placeholder="Stars"
+                    onChange={this.update('rating')} />
+                </div>
+                <div className="recipe-form-part2">
+                  <span>Website</span>
+                  <input className="input-field" type="text" value={this.state.link} placeholder="URL"
+                    onChange={this.update('link')} />
+                </div>
               </div>
             </div>
-            <div className="recipe-form-part3">
-              <div className="recipe-form-part2">
-                <span>Servings</span>
-                <input className="input-field" type="number" value={this.state.serving} placeholder="Number of Servings"
-                  onChange={this.update('serving')} />
-              </div>
-              <div className="recipe-form-part2">
-                <span>Rating</span>
-                <input className="input-field" type="number" value={this.state.rating} placeholder="Stars"
-                  onChange={this.update('rating')} />
-              </div>
-              <div className="recipe-form-part2">
-                <span>Website</span>
-                <input className="input-field" type="text" value={this.state.link} placeholder="URL"
-                  onChange={this.update('link')} />
-              </div>
+
+            <div>
+              <span>Description</span>
+              <textarea name="description" value={this.state.description}
+                placeholder="Short Description" rows="3"
+                onChange={this.update('description')} />
             </div>
-          </div>
 
-          <div>
-            <span>Description</span>
-            <textarea name="description" value={this.state.description}
-              placeholder="Short Description" rows="3"
-              onChange={this.update('description')} />
-          </div>
+            <div>
+              <span>Ingredients</span>
+              <textarea name="ingredients" value={this.state.ingredients}
+                placeholder="Ingredients" rows="7"
+                onChange={this.update('ingredients')} />
+            </div>
 
-          <div>
-            <span>Ingredients</span>
-            <textarea name="ingredients" value={this.state.ingredients}
-              placeholder="Ingredients" rows="7"
-              onChange={this.update('ingredients')} />
-          </div>
+            <div>
+              <span>Directions</span>
+              <textarea name="directions" value={this.state.directions}
+                placeholder="Directions" rows="7"
+                onChange={this.update('directions')} />
+            </div>
+            <div>
+              <span>Notes</span>
+              <textarea name="notes" value={this.state.notes}
+                placeholder="Cooking Notes" rows="3"
+                onChange={this.update('notes')} />
+            </div>
 
-          <div>
-            <span>Directions</span>
-            <textarea name="directions" value={this.state.directions}
-              placeholder="Directions" rows="7"
-              onChange={this.update('directions')} />
-          </div>
-          <div>
-            <span>Notes</span>
-            <textarea name="notes" value={this.state.notes}
-              placeholder="Cooking Notes" rows="3"
-              onChange={this.update('notes')} />
-          </div>
+            <div>
+              <span></span>
+              <button className="recipe-form-button">Save Recipe</button>
+            </div>
+          </section>
 
-          <div>
-            <span></span>
-            <button className="recipe-form-button">Save Recipe</button>
-          </div>
-        </section>
-
-      </form>
+        </form>
+      </div>
     )
   }
 }

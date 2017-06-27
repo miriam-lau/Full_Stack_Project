e59e5f6aa99d370f2ec286dcc6415c4710aec678
Miriam Lau <meiyen.lau@gmail.com>
2017-06-27 15:02:55 -0700

    Update database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,6 @@ pantry9 = PantryItem.create(category: "Fruits and Vegetables", name: "Cucumber",
 pantry10 = PantryItem.create(category: "Fruits and Vegetables", name: "Carrot",
   quantity: 1, unit: "pound", user_id: guest.id)
 
-  
 
 grocery1 = Grocery.create(category: "Fruits and Vegetables", name: "Banana",
   quantity: 2, unit: "", user_id: guest.id, purchased: false)

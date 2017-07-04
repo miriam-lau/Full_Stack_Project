8dd7167bc5e26da776f97e58ad4b99c1c8744968
Miriam Lau <meiyen.lau@gmail.com>
2017-07-03 17:56:57 -0700

    Refactor CSS styling on tutorial

--- frontend/components/application.jsx
+++ frontend/components/application.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { AuthRoute, Link } from '../util/route_util';
-
-import MainContainer from './main/main_container';
-import HomeContainer from './home/home_container';
-import PantryIndexContainer from './pantry_items/pantry_index_container';
-import ModalContainer from './modal/modal_container';
-import SearchResultsContainer from './search/search_results_container';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import MainContainer from './main/main_container';
+
 injectTapEventPlugin();
+
 const App = () => {
   return (
     <div>
--- frontend/components/tutorial/tutorial.jsx
+++ frontend/components/tutorial/tutorial.jsx
@@ -30,41 +30,43 @@ class Tutorial extends React.Component {
 
             <div className="drawer-text-guide">Guide</div>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Navigation: &nbsp;</span> Click between tabs on the left.</p>
-            </section>
+            <div className="tutorial">
+              <section>
+                <p><span>Navigation: &nbsp;</span> Click between tabs on the left.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Search: &nbsp;</span>Click the icon located in the header.</p>
-            </section>
+              <section>
+                <p><span>Search: &nbsp;</span>Click the icon located in the header.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Add: &nbsp;</span>Type in a number, unit(optional), and name. Press "Enter" or click +.</p>
-            </section>
+              <section>
+                <p><span>Add: &nbsp;</span>Type in a number, unit(optional), and name. Press "Enter" or click +.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Duplicate Items: &nbsp;</span>Adding an item with same name and unit will auto-update the same item in pantry.</p>
-            </section>
+              <section>
+                <p><span>Duplicate Items: &nbsp;</span>Adding an item with same name and unit will auto-update the same item in pantry.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Update: &nbsp;</span>Click on the item fields. Click off the field to save.</p>
-            </section>
+              <section>
+                <p><span>Update: &nbsp;</span>Click on the item fields. Click off the field to save.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Delete: &nbsp;</span>Click on the "garbage can" icon.</p>
-            </section>
+              <section>
+                <p><span>Delete: &nbsp;</span>Click on the "garbage can" icon.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Moving Items (Grocery only): &nbsp;</span>Check/uncheck to move an item to "Purchased" and vice versa.</p>
-            </section>
+              <section>
+                <p><span>Moving Items (Grocery only): &nbsp;</span>Check/uncheck to move an item to "Purchased" and vice versa.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">"Purchased" Items (Grocery only): &nbsp;</span>Items can also be updated and deleted.</p>
-            </section>
+              <section>
+                <p><span>"Purchased" Items (Grocery only): &nbsp;</span>Items can also be updated and deleted.</p>
+              </section>
 
-            <section className="tutorial">
-              <p><span className="tutorial-topic">Auto-add to Pantry (Grocery only): &nbsp;</span>Click "Add to Pantry" to move "Purchased" items to pantry.</p>
-            </section>
+              <section>
+                <p><span>Auto-add to Pantry (Grocery only): &nbsp;</span>Click "Add to Pantry" to move "Purchased" items to pantry.</p>
+              </section>
+            </div>
           </Drawer> : ""
         }
       </div>

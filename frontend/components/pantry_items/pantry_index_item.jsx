e6a3c566d2aea856819c87cb9c616761419e1497
Miriam Lau <meiyen.lau@gmail.com>
2017-07-05 01:14:07 -0700

    Update styling for pantry_index_item and update_pantry components

--- frontend/components/pantry_items/pantry_index_item.jsx
+++ frontend/components/pantry_items/pantry_index_item.jsx
@@ -7,12 +7,12 @@ import { allMeasurements } from "../utils/measurements";
 import { formCategory } from "../utils/item_categories";
 import { FontIcon, TextField } from "material-ui/";
 import { underlineStyle, underlineFocusStyle, quantityStyle, itemStyleDefault,
-  itemStyleCategory, styles } from "../utils/material_ui_styles";
+    itemStyleCategory, styles } from "../utils/material_ui_styles";
 
 class PantryIndexItem extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { quantityError: "", nameError: ""};
+    this.state = { quantityError: "", nameError: "" };
 
     this.parseUpdateQuantity = this.parseUpdateQuantity.bind(this);
     this.showQuantityError = this.showQuantityError.bind(this);
@@ -25,7 +25,7 @@ class PantryIndexItem extends React.Component {
     let firstNum = /(^\d+(?:\.\d+)?)/;
     let splitFirstWord = words.shift().split(firstNum);
     if (splitFirstWord.length === 1) {
-      return {error: "Quantity must be a number"};
+      return { error: "Quantity must be a number" };
     }
 
     words = splitFirstWord.concat(words);
@@ -38,38 +38,41 @@ class PantryIndexItem extends React.Component {
     let convertedUnit = null;
 
     if (unit != null) {
-      if (unit[unit.length - 1] == ".") {
+      if (unit[unit.length - 1] === ".") {
         unit = unit.substring(0, unit.length - 1);
       }
       for (let i = 0; i < allMeasurements.length; i++) {
         if (allMeasurements[i].includes(unit)) {
-          convertedUnit = (quantity === "1" ? allMeasurements[i][0] : allMeasurements[i][1]);
+          convertedUnit =
+              (quantity === "1" ?
+               allMeasurements[i][0] :
+               allMeasurements[i][1]);
           break;
         }
       }
     }
 
-    if (convertedUnit === null && unit != null) {
-      return {error: "Invalid unit"};
+    if (convertedUnit == null && unit != null) {
+      return { error: "Invalid unit" };
     }
-    return {quantity: parseFloat(quantity), unit: convertedUnit};
+    return { quantity: parseFloat(quantity), unit: convertedUnit };
   }
 
-  //write what function does
+  // TODO: write what function does
   handleQuantityChange() {
     return e => {
       let parsedUpdateQuantity = this.parseUpdateQuantity(e.target.value);
       if (parsedUpdateQuantity.error != null) {
         this.props.updateQuantityDisplay(this.props.pantryItem.id,
             e.target.value);
-        this.setState({quantityError: parsedUpdateQuantity.error});
+        this.setState({ quantityError: parsedUpdateQuantity.error });
       } else {
-        this.setState({quantityError: ""});
+        this.setState({ quantityError: "" });
         let updatedPantryItem = merge({}, this.props.pantryItem);
         updatedPantryItem.currentQuantityDisplay = e.target.value;
         updatedPantryItem.quantity = parsedUpdateQuantity.quantity;
         updatedPantryItem.unit = parsedUpdateQuantity.unit;
-        this.props.updatePantryItem({pantry_item: updatedPantryItem});
+        this.props.updatePantryItem({ pantry_item: updatedPantryItem });
       }
     }
   }
@@ -78,93 +81,90 @@ class PantryIndexItem extends React.Component {
     return e => {
       if (property === "name") {
         if (e.target.value === "") {
-          return this.setState({nameError: "Name cannot be blank"});
+          this.setState({ nameError: "Name cannot be blank" });
+          return;
         } else {
-          this.setState({nameError: ""});
+          this.setState({ nameError: "" });
         }
       }
 
-      /*
-        Only for existing uncategorized items; returns true if updated
-        @param pantryItems{array}
-        @param name{string}
-        @param category{string}
-        @param unit{string}
-        @param quantity{float}
-        //change function name updatePantry
-      */
       if (property === "category") {
-        console.log(pantryItem);
         let pantryItem = this.props.pantryItem;
-        if (updatePantry(this.props.pantryItems, pantryItem.id,
-            pantryItem.name, e.target.value, pantryItem.unit, parseFloat(pantryItem.quantity), this.props.updatePantryItem, this.props.deletePantryItem)) {
+        if (updatePantry(
+            this.props.pantryItems,
+            pantryItem.id,
+            pantryItem.name,
+            e.target.value,
+            pantryItem.unit,
+            parseFloat(pantryItem.quantity),
+            this.props.updatePantryItem,
+            this.props.deletePantryItem)) {
           return;
         }
       };
 
       let newPantryItem = merge({}, this.props.pantryItem);
       newPantryItem[property] = e.target.value;
-      this.props.updatePantryItem({pantry_item: newPantryItem});
+      this.props.updatePantryItem({ pantry_item: newPantryItem });
     }
   }
 
-  showQuantityError(message) {
-    if (message != "") {
-      return (<div className="pantry-item-error">{ message }</div>);
+  showQuantityError() {
+    if (this.state.quantityError === "") {
+      return null;
     }
-    return null;
+    return(
+      <div className="pantry-item-error">{ this.state.quantityError }</div>
+    );
   }
 
-  showNameError(message) {
-    if (message != "" && this.state.quantityError === "") {
-      return (<div className="pantry-item-name-error-only">{ message }</div>);
+  showNameError() {
+    if (this.state.nameError === "") {
+      return null;
     }
-    if (message != "") {
-      return (<div className="pantry-item-name-error">{ message }</div>);
+    if (this.state.quantityError === "") {
+      return (
+        <div className="pantry-item-name-error-only">{ this.state.nameError }
+        </div>
+      );
     }
-    return null;
+    return (
+      <div className="pantry-item-name-error">{ this.state.nameError }</div>
+    );
   }
 
   render() {
     const pantryItem = this.props.pantryItem;
-    const deletePantryItem = this.props.deletePantryItem;
-
     return (
       <div>
         <div className="update-pantry-form-div">
           <form className="update-pantry-form">
             <TextField id="text-field-default"
               value={ pantryItem.currentQuantityDisplay }
-              underlineFocusStyle ={underlineFocusStyle} underlineStyle={underlineStyle}
-              style={quantityStyle}
-              onChange={this.handleQuantityChange()}
+              underlineFocusStyle={ underlineFocusStyle }
+              underlineStyle={ underlineStyle }
+              style={ quantityStyle }
+              onChange={ this.handleQuantityChange() }
+            />
+
+            <TextField id="text-field-default"
+              value={ pantryItem.name }
+              underlineFocusStyle={ underlineFocusStyle }
+              underlineStyle={ underlineStyle }
+              onChange={ this.update("name") }
+              style = { pantryItem.category === "" ?
+                  itemStyleCategory : itemStyleDefault }
             />
 
             {pantryItem.category === "" ?
-              <TextField id="text-field-default"
-                value={ pantryItem.name }
-                underlineFocusStyle ={underlineFocusStyle}
-                underlineStyle={underlineStyle}
-                style={itemStyleCategory}
-                onChange={this.update("name")}
-              /> :
-              <TextField id="text-field-default"
-                value={ pantryItem.name }
-                underlineFocusStyle ={underlineFocusStyle}
-                underlineStyle={underlineStyle}
-                style={itemStyleDefault}
-                onChange={this.update("name")}
-              />
-            }
-
-            {pantryItem.category === "" ?
               <select className="pantry-uncategorized"
-                onChange={this.update("category")}>
+                  onChange={ this.update("category") }>
                 <option selected="true" disabled="disabled">
-                    Select a Category</option>
+                  Select a Category
+                </option>
                 {formCategory.map((category, idx) => {
-                  return(
-                    <option key={idx} value={category}>{category}</option>
+                  return (
+                    <option key={ idx } value={ category }>{ category }</option>
                   )
                 })};
               </select> : ""}
@@ -172,14 +172,14 @@ class PantryIndexItem extends React.Component {
 
           <i className="material-icons trash-can"
             style={styles}
-            onClick={() => deletePantryItem(pantryItem.id)}>
+            onClick={ () => this.props.deletePantryItem(pantryItem.id) }>
             delete_forever
           </i>
         </div>
 
         <div className="error-messages">
-          {this.showQuantityError(this.state.quantityError)}
-          {this.showNameError(this.state.nameError)}
+          { this.showQuantityError() }
+          { this.showNameError() }
         </div>
       </div>
     );
--- frontend/components/pantry_items/update_pantry.jsx
+++ frontend/components/pantry_items/update_pantry.jsx
@@ -3,17 +3,14 @@ import React from "react";
 /*
   Checks if current item is already in pantryItems, and updates the item if it is found, and deletes the current item if it already exists.
 
-  @param allItems{array} all pantryItems
-  @param id{integer} current item id, -1 for non-existing items
-  @param item{string} current item name
-  @param category{string} current item category
-  @param convertedUnit{string} current item category
-  @param quantity{float} current item quantity
-  @return{boolean} true if a match is found and updated, otherwise false
+  @param allItems {array} all pantryItems
+  @param id {integer} current item id, -1 for non-existing items
+  @param item {string} current item name
+  @param category {string} current item category
+  @param convertedUnit {string} current item category
+  @param quantity {float} current item quantity
+  @return {boolean} true if a match is found and updated, otherwise false
 */
-
-
-
 const updatePantry = (allItems, id, item, category, convertedUnit, quantity,
     updatePantryItem, deletePantryItem) => {
 
@@ -32,6 +29,7 @@ const updatePantry = (allItems, id, item, category, convertedUnit, quantity,
     } else if (itemUnit.charAt(itemUnit.length - 1) === "s") {
       itemUnit = itemUnit.substring(0, (itemUnit.length - 1));
     }
+
     // set current item unit to singular
     if (convertedUnit === "inch" || convertedUnit === "inches") {
       convertedUnit = "inch";
@@ -40,6 +38,7 @@ const updatePantry = (allItems, id, item, category, convertedUnit, quantity,
     } else if (convertedUnit.charAt(convertedUnit.length - 1) === "s") {
       convertedUnit = convertedUnit.substring(0, (convertedUnit.length - 1));
     }
+
     // go to next item if unit does not match current unit
     // if there is match, add item quantity to current item quantity
     if (convertedUnit !== itemUnit) {
@@ -58,6 +57,7 @@ const updatePantry = (allItems, id, item, category, convertedUnit, quantity,
         convertedUnit += "s";
       }
     }
+
     // set the currentQuantityDisplay
     let currentQuantityDisplay = quantity;
     if (convertedUnit != null) {
@@ -65,14 +65,19 @@ const updatePantry = (allItems, id, item, category, convertedUnit, quantity,
     }
 
     let pantryItem = {
-        id: allItems[i].id, name: item, category: allItems[i].category, quantity: quantity, unit: convertedUnit, currentQuantityDisplay: currentQuantityDisplay
-      };
+        id: allItems[i].id,
+        name: item,
+        category: allItems[i].category,
+        quantity: quantity,
+        unit: convertedUnit,
+        currentQuantityDisplay: currentQuantityDisplay
+    };
 
     if (id === -1) {
-      updatePantryItem({pantry_item: pantryItem});
+      updatePantryItem({ pantry_item: pantryItem });
     } else {
-      updatePantryItem({pantry_item: pantryItem}).then(() =>
-          deletePantryItem(id));
+      updatePantryItem({ pantry_item: pantryItem })
+          .then(() => deletePantryItem(id));
     }
     return true;
   }
